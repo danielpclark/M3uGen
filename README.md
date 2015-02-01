6c931f804bf66455d0c9b9edba73c283a6fcdd09
Daniel P. Clark <6ftdan@gmail.com>
2015-02-01 12:44:14 -0500

    Prepping.

---
+++ README.md
@@ -0,0 +1,1 @@
+Dynamically generate playlists to stream audio files.
---
+++ lib/m3u_gen.rb
@@ -0,0 +1,20 @@
+class M3uGen
+  def initialize(path = '/'); @path = path; end
+  def path=(path); @path = path; end
+  def path; @path; end
+
+  def parse_dir(dir = @path)
+    result = {}
+    Dir.glob("#{dir}/**/*.mp3").map {|path|
+      m3u = "#{path.split('/')[-2].gsub(' ','_').downcase}.m3u"
+      result[m3u] = Hash(result[m3u]).update({
+        title: path.split('/')[-2] || "Unsorted",
+        files: Array(Hash(result[m3u])[:files]).<<({
+          path: path,
+          filename: path.split('/').last,
+        })
+      })
+    }
+    result
+  end
+end
